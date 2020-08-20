c3be15cbb02080b43b1201b284e1e61c61284481
MoKi <61506669+moki929@users.noreply.github.com>
2020-08-20 14:30:26 -0400

    watching video

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
 
+const API = {
+  apiKey: "",
+  base: "https://api.openweathermap.org/data/2.5/" 
+}
+
 function App() {
   return (
     <div className="App">
